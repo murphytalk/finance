fe0c128bc0e2028e63b41d879c22e1c2efef41a6
Mu Lu <murphytalk@gmail.com>
2016-02-13 17:13:41 +0900

    DAO

--- src/main/python/calculate.py
+++ src/main/python/calculate.py
@@ -9,28 +9,14 @@ parameter:
 """
 import sys,sqlite3
 from datetime import date
-from time import mktime
 from utils import cmdline_args
 from adapter import *
-
-
-def iterate_transaction(db_path,start_date,end_date,callback):
-    conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row
-    c = conn.cursor()
-
-    epoch1 = int(mktime(start_date.timetuple())+get_utc_offset())
-    epoch2 = int(mktime(end_date.timetuple())+get_utc_offset())
-
-    c.execute('select i.name,t.instrument,t.type,t.price,t.shares,t.fee,t.date from [transaction] t, instrument i where t.instrument = i.rowid and date >=? and date<=? order by date',(epoch1,epoch2))
-    for f in c.fetchall():
-        callback(f['instrument'],f['name'],f['type'],f['price'],f['shares'],f['fee'],f['date'])
-    conn.close()
-
+from dao import Dao
 
 class Position:
-    def __init__(self,instrument):
+    def __init__(self,instrument,name):
         self.instrument = instrument
+        self.name = name
         self.shares = 0
         self.liquidated = 0 # negative value => money(original value) still in market
         self.fee = 0
@@ -46,9 +32,8 @@ class Position:
                 self.fee = self.fee + fee
 
     def __str__(self):
-        return "Instrument=%d,Shares=%d,Fee=%d,Liquidated=%d"%(self.instrument,self.shares,self.fee,self.liquidated)
+        return "Instrument=%d,Name=%s,Shares=%d,Fee=%d,Liquidated=%d"%(self.instrument,self.name,self.shares,self.fee,self.liquidated)
         
-
 class CalcPosition:
     def __init__(self,date1,date2):
         self.date1 = date1
@@ -56,19 +41,14 @@ class CalcPosition:
         
     def calc(self,dbpath,no_update_db):
         def on_each_transaction(instrument,name,transaction_type,price,shares,fee,date):
-            self.positions[name].transaction(transaction_type,price,shares,fee)
+            self.positions[instrument].transaction(transaction_type,price,shares,fee)
 #            if not no_update_db:
                 
 
-            
-        conn = sqlite3.connect(dbpath)
-        conn.row_factory = sqlite3.Row
-        c = conn.cursor()
-        c.execute('select rowid,[name] from instrument where type = 2 or type = 1')
-        conn.close
-        self.positions = { x[1]:Position(x[0]) for x in c.fetchall() }
-        
-        iterate_transaction(dbpath,self.date1,self.date2,on_each_transaction)
+        d = Dao(dbpath)
+        self.positions = d.populate_from_intruments('type = 2 or type = 1',lambda id,name : Position(id,name))
+        d.iterate_transaction(self.date1,self.date2,on_each_transaction)
+        d.close()
 
     def dump(self):
         for k,v in self.positions.iteritems():
---
+++ src/main/python/dao.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+from time import mktime
+from utils import get_utc_offset
+
+class Dao:
+    def __init__(self,dbpath):
+        self.conn = sqlite3.connect(dbpath)
+        self.conn.row_factory = sqlite3.Row
+        self.c = self.conn.cursor()
+
+    def close(self):
+        self.conn.close()
+        
+    def iterate_transaction(self,start_date,end_date,callback):
+        """
+        iterate stock transactions, callback signature:
+        callback(instrument id,instrument name,transaction type,price, shares,fee, date)
+        """
+        epoch1 = int(mktime(start_date.timetuple())+get_utc_offset())
+        epoch2 = int(mktime(end_date.timetuple())+get_utc_offset())
+    
+        self.c.execute('select i.name,t.instrument,t.type,t.price,t.shares,t.fee,t.date from [transaction] t, instrument i where t.instrument = i.rowid and date >=? and date<=? order by date',(epoch1,epoch2))
+        for f in self.c.fetchall():
+            callback(f['instrument'],f['name'],f['type'],f['price'],f['shares'],f['fee'],f['date'])
+        
+    def populate_from_intruments(self,filter,create_new_obj_func):
+        """
+        Populate a dict {instrument id : user defined data type} by
+        querying from table instrument , filter should be a valid SQL where clause
+        
+        create_new_obj_func signature:
+        parameter:
+          instrument id
+          instrument name
+        return:
+          the new object
+
+        """
+        sql = 'SELECT ROWID,[NAME] FROM INSTRUMENT'
+        if filter is not None:
+            sql = sql + ' where ' + filter
+        self.c.execute(sql)
+        return {x[0]:create_new_obj_func(x[0],x[1]) for x in self.c.fetchall()}
+
