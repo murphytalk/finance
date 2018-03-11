89657581aea05bf71d16ffa49f327b71161c710b
Mu Lu <murphytalk@gmail.com>
2018-03-11 16:11:58 +0900

    position unit test: corp act - split

--- src/test.py
+++ src/test.py
@@ -14,14 +14,26 @@ class TestPosition(unittest.TestCase):
             p.transaction(trans[0], trans[1], trans[2], trans[3])
         self.assertAlmostEqual(p.VWAP(), 293.333, 3)
 
+    def test_split(self):
+        p = Position(1, 'test')
+        for trans in [
+            ('BUY', 100, 10, 0),
+            ('BUY', 200, 10, 0),
+            ('BUY', 300, 20, 0),
+            ('BUY', 400, 30, 0),
+            ('SPLIT', 2, 0, 0)]:
+            p.transaction(trans[0], trans[1], trans[2], trans[3])
+        self.assertEqual(p.VWAP(), 150)
+
     def test_buy_sell(self):
         p = Position(1, 'test')
         for trans in [
             ('BUY', 100, 10, 0),
             ('BUY', 200, 15, 0),
-            ('SELL', 300, 20, 0),
+            ('SELL', 150, 20, 0),
             ('BUY', 400, 30, 0)]:
             p.transaction(trans[0], trans[1], trans[2], trans[3])
+        self.assertAlmostEqual(p.VWAP(), 371.4286, 4)
 
 
 if __name__ == '__main__':
