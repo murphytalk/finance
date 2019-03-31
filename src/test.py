e99bc97d670638450afa29f7cb5d7c2b68cb9910
Mu Lu <murphytalk@gmail.com>
2019-03-31 12:16:19 +0900

    build by travis

--- src/test.py
+++ src/test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import unittest
 from datetime import timedelta
 
@@ -12,43 +12,47 @@ YYYY_MM_DD = "%Y-%m-%d"
 
 class TestPosition(unittest.TestCase):
     def test_buy(self):
-        p = Position(1, 'test')
+        p = Position(1, "test")
         for trans in [
-            ('BUY', 100, 10, 0),
-            ('BUY', 200, 15, 0),
-            ('BUY', 300, 20, 0),
-            ('BUY', 400, 30, 0)]:
+            ("BUY", 100, 10, 0),
+            ("BUY", 200, 15, 0),
+            ("BUY", 300, 20, 0),
+            ("BUY", 400, 30, 0),
+        ]:
             p.transaction(trans[0], trans[1], trans[2], trans[3])
         self.assertAlmostEqual(p.VWAP(), 293.333, 3)
 
     def test_split(self):
-        p = Position(1, 'test')
+        p = Position(1, "test")
         for trans in [
-            ('BUY', 100, 10, 0),
-            ('BUY', 200, 10, 0),
-            ('BUY', 300, 20, 0),
-            ('BUY', 400, 30, 0),
-            ('SPLIT', 2, 0, 0)]:
+            ("BUY", 100, 10, 0),
+            ("BUY", 200, 10, 0),
+            ("BUY", 300, 20, 0),
+            ("BUY", 400, 30, 0),
+            ("SPLIT", 2, 0, 0),
+        ]:
             p.transaction(trans[0], trans[1], trans[2], trans[3])
         self.assertEqual(p.VWAP(), 150)
 
     def test_buy_sell(self):
-        p = Position(1, 'test')
+        p = Position(1, "test")
         for trans in [
-            ('BUY', 100, 10, 0),
-            ('BUY', 200, 15, 0),
-            ('SELL', 150, 20, 0),
-            ('BUY', 400, 30, 0)]:
+            ("BUY", 100, 10, 0),
+            ("BUY", 200, 15, 0),
+            ("SELL", 150, 20, 0),
+            ("BUY", 400, 30, 0),
+        ]:
             p.transaction(trans[0], trans[1], trans[2], trans[3])
         self.assertAlmostEqual(p.VWAP(), 371.4286, 4)
 
     def test_closed_position(self):
-        p = Position(1, 'test')
+        p = Position(1, "test")
         for trans in [
-            ('BUY', 100, 10, 0),
-            ('BUY', 200, 15, 0),
-            ('SELL', 150, 20, 0),
-            ('SELL', 150, 5, 0)]:
+            ("BUY", 100, 10, 0),
+            ("BUY", 200, 15, 0),
+            ("SELL", 150, 20, 0),
+            ("SELL", 150, 5, 0),
+        ]:
             p.transaction(trans[0], trans[1], trans[2], trans[3])
         self.assertEqual(p.VWAP(), 0)
 
@@ -67,7 +71,9 @@ class TestDao(unittest.TestCase):
         self.countries = self._raw_queryset_with_id_to_set(self.dao.get_countries())
         self.country_name_to_id = {n: i for i, n in self.dao.get_countries()}
         self.asset_types = self._raw_queryset_with_id_to_set(self.dao.get_asset_types())
-        self.instrument_types = self._raw_queryset_with_id_to_set(self.dao.get_instrument_types())
+        self.instrument_types = self._raw_queryset_with_id_to_set(
+            self.dao.get_instrument_types()
+        )
         self.brokers = self.dao.get_broker_mapper()
         self.ccy = self.dao.get_currency_mapper()
         self.filters = self.dao.get_filters()
@@ -77,48 +83,58 @@ class TestDao(unittest.TestCase):
     def test_reference_data(self):
         self.assertEqual(
             self.countries,
-            {'Africa Other',
-             'Asia Other',
-             'Australia',
-             'Canada',
-             'China',
-             'E.Europe Other',
-             'Europe placeholder',
-             'France',
-             'Germany',
-             'Hong Kong',
-             'India',
-             'Japan',
-             'Middle East Other',
-             'NZ',
-             'Other',
-             'S.America Other',
-             'S.Korea',
-             'Singapore',
-             'Switzerland',
-             'Taiwan',
-             'UK',
-             'US',
-             'W.Europe Other',
-             'placeholder'}
+            {
+                "Africa Other",
+                "Asia Other",
+                "Australia",
+                "Canada",
+                "China",
+                "E.Europe Other",
+                "Europe placeholder",
+                "France",
+                "Germany",
+                "Hong Kong",
+                "India",
+                "Japan",
+                "Middle East Other",
+                "NZ",
+                "Other",
+                "S.America Other",
+                "S.Korea",
+                "Singapore",
+                "Switzerland",
+                "Taiwan",
+                "UK",
+                "US",
+                "W.Europe Other",
+                "placeholder",
+            },
         )
-        self.assertEqual(self.asset_types, {'Cash', 'Gold', 'REIT', 'Corp Bond', 'Government Bond', 'Other', 'Stock'})
-        self.assertEqual(self.brokers, {'ABC': 1, 'IB': 3, 'XYZ': 2})
-        self.assertEqual(self.instrument_types, {'ETF', 'Funds', 'Crypto', 'Stock', 'Bond'})
-        self.assertEqual(self.ccy, {'AUD': 5, 'CNY': 4, 'EUR': 3, 'HKD': 7, 'JPY': 1, 'NZD': 6, 'USD': 2})
-        self.assertIn('All-Stocks', self.filters)
-        self.assertIn('First-Two-Stocks', self.filters)
-        self.assertIn('First-Stock-Reduce-100-Shares', self.filters)
+        self.assertEqual(
+            self.asset_types,
+            {"Cash", "Gold", "REIT", "Corp Bond", "Government Bond", "Other", "Stock"},
+        )
+        self.assertEqual(self.brokers, {"ABC": 1, "IB": 3, "XYZ": 2})
+        self.assertEqual(
+            self.instrument_types, {"ETF", "Funds", "Crypto", "Stock", "Bond"}
+        )
+        self.assertEqual(
+            self.ccy,
+            {"AUD": 5, "CNY": 4, "EUR": 3, "HKD": 7, "JPY": 1, "NZD": 6, "USD": 2},
+        )
+        self.assertIn("All-Stocks", self.filters)
+        self.assertIn("First-Two-Stocks", self.filters)
+        self.assertIn("First-Stock-Reduce-100-Shares", self.filters)
         for n, f in self.filters.items():
-            self.assertIn('extra', f)
-            self.assertIn('instruments', f)
+            self.assertIn("extra", f)
+            self.assertIn("instruments", f)
 
         regions = self.dao.get_country_region_lookup()
-        self.assertEqual(regions[self.country_name_to_id['US']], 'N.America')
-        self.assertEqual(regions[self.country_name_to_id['UK']], 'W.Europe')
-        self.assertEqual(regions[self.country_name_to_id['Japan']], 'Asia')
-        self.assertEqual(regions[self.country_name_to_id['China']], 'Asia')
-        self.assertEqual(regions[self.country_name_to_id['Canada']], 'N.America')
+        self.assertEqual(regions[self.country_name_to_id["US"]], "N.America")
+        self.assertEqual(regions[self.country_name_to_id["UK"]], "W.Europe")
+        self.assertEqual(regions[self.country_name_to_id["Japan"]], "Asia")
+        self.assertEqual(regions[self.country_name_to_id["China"]], "Asia")
+        self.assertEqual(regions[self.country_name_to_id["Canada"]], "N.America")
 
     def test_instruments(self):
         # all generated instruments
@@ -127,9 +143,9 @@ class TestDao(unittest.TestCase):
             self.assertEqual(instrument.url, URL)
 
             i = self.dao.get_instruments(instrument.name).__next__()
-            self.assertIn(i['currency'], self.ccy)
-            self.assertIn(i['broker'], self.brokers)
-            self.assertEqual(i['name'], instrument.name)
+            self.assertIn(i["currency"], self.ccy)
+            self.assertIn(i["broker"], self.brokers)
+            self.assertEqual(i["name"], instrument.name)
 
             for a in self.dao.get_asset_allocation(instrument_name=instrument.name):
                 self.assertIn(a[0], self.asset_types)
@@ -144,7 +160,9 @@ class TestDao(unittest.TestCase):
             self.assertTrue(epoch2date(TODAY) >= quote.date)
             self.assertTrue(epoch2date(DAY1) <= quote.date)
 
-        for iid, instrument in self.dao.get_instrument_with_xccy_rate(epoch2date(TODAY)).items():
+        for iid, instrument in self.dao.get_instrument_with_xccy_rate(
+            epoch2date(TODAY)
+        ).items():
             self.assertIn(iid, self.instruments)
             self.assertIn(instrument.instrument_type.name, self.instrument_types)
             self.assertIn(instrument.currency, self.ccy)
@@ -152,33 +170,46 @@ class TestDao(unittest.TestCase):
 
     def test_funds(self):
         for pos in self.dao.get_funds_positions(TODAY):
-            self.assertTrue(pos['instrument_id'] in self.instruments)
-            self.assertEqual(self.instruments[pos['instrument_id']].instrument_type.name, 'Funds')
+            self.assertTrue(pos["instrument_id"] in self.instruments)
+            self.assertEqual(
+                self.instruments[pos["instrument_id"]].instrument_type.name, "Funds"
+            )
             # self.assertTrue(pos['currency'] in self.ccy)
-            self.assertIn(pos['broker'], self.brokers)
-            self.assertIsInstance(pos['amount'], int)
-            self.assertIsInstance(pos['value'], float)
-            self.assertIsInstance(pos['price'], float)
-            self.assertIsInstance(pos['profit'], float)
-            self.assertIsInstance(pos['capital'], float)
+            self.assertIn(pos["broker"], self.brokers)
+            self.assertIsInstance(pos["amount"], int)
+            self.assertIsInstance(pos["value"], float)
+            self.assertIsInstance(pos["price"], float)
+            self.assertIsInstance(pos["profit"], float)
+            self.assertIsInstance(pos["capital"], float)
 
     def test_stocks(self):
-        stock = get_random_dict_value(self.instruments,
-                                      lambda x: True if x.instrument_type.name == 'Stock' else False)
+        stock = get_random_dict_value(
+            self.instruments,
+            lambda x: True if x.instrument_type.name == "Stock" else False,
+        )
         fee = 9.99
         shares = 99
         price = 9.1234
-        trans_type = 'SELL'
+        trans_type = "SELL"
         dt = (epoch2date(TODAY) + timedelta(days=1)).strftime(YYYY_MM_DD)
-        self.assertTrue(self.dao.update_stock_transaction(stock.name, {"Price": price, "Type": trans_type,
-                                                                       "Date": dt, "Fee": fee,
-                                                                       "Shares": shares}))
+        self.assertTrue(
+            self.dao.update_stock_transaction(
+                stock.name,
+                {
+                    "Price": price,
+                    "Type": trans_type,
+                    "Date": dt,
+                    "Fee": fee,
+                    "Shares": shares,
+                },
+            )
+        )
         for trans in self.dao.get_stock_transaction(stock.name):
-            self.assertEqual(trans['symbol'], stock.name)
-            self.assertIn(trans['type'], {'BUY', 'SELL', 'SPLIT'})
-            self.assertIsInstance(trans['price'], float)
-            self.assertIsInstance(trans['fee'], float)
-            self.assertIsInstance(trans['shares'], float)
+            self.assertEqual(trans["symbol"], stock.name)
+            self.assertIn(trans["type"], {"BUY", "SELL", "SPLIT"})
+            self.assertIsInstance(trans["price"], float)
+            self.assertIsInstance(trans["fee"], float)
+            self.assertIsInstance(trans["shares"], float)
             if trans["date"] == dt:
                 # the one just added
                 self.assertEqual(trans["fee"], fee)
@@ -188,37 +219,39 @@ class TestDao(unittest.TestCase):
 
 
 def test_update_instrument(self):
-    instrument = get_random_dict_value(self.instruments,
-                                       lambda x: True if x.instrument_type.name == 'Funds' else False)
-    url = 'http://my.new.com/url'
+    instrument = get_random_dict_value(
+        self.instruments, lambda x: True if x.instrument_type.name == "Funds" else False
+    )
+    url = "http://my.new.com/url"
     expense = 9.99
-    ccy = 'CNY'
+    ccy = "CNY"
     broker = get_random_dict_key(self.brokers)
 
-    payload = {
-        'currency': ccy,
-        'broker': broker,
-        'url': url,
-        'expense': expense
-    }
+    payload = {"currency": ccy, "broker": broker, "url": url, "expense": expense}
 
     # update existing
     self.assertTrue(self.dao.update_instrument(instrument.name, payload))
     i = self.dao.get_instruments(instrument.name).__next__()
-    self.assertEqual(i['type'], 'Funds')
-    self.assertEqual(i['currency'], ccy)
-    self.assertEqual(i['broker'], broker)
-    self.assertEqual(i['url'], url)
-    self.assertEqual(i['expense'], expense)
+    self.assertEqual(i["type"], "Funds")
+    self.assertEqual(i["currency"], ccy)
+    self.assertEqual(i["broker"], broker)
+    self.assertEqual(i["url"], url)
+    self.assertEqual(i["expense"], expense)
 
     # update allocations
     # asset allocation
     assets = set(self.asset_types)
     a1 = assets.pop()
     a2 = assets.pop()
-    self.assertTrue(self.dao.update_instrument_asset_allocations(instrument.name, {
-        'assets': [{'asset': a1, 'ratio': 20}, {'asset': a2, 'ratio': 80}]}))
-    a = {t: r for t, r in self.dao.get_asset_allocation(instrument_name=instrument.name)}
+    self.assertTrue(
+        self.dao.update_instrument_asset_allocations(
+            instrument.name,
+            {"assets": [{"asset": a1, "ratio": 20}, {"asset": a2, "ratio": 80}]},
+        )
+    )
+    a = {
+        t: r for t, r in self.dao.get_asset_allocation(instrument_name=instrument.name)
+    }
     self.assertEqual(a[a1], 20)
     self.assertEqual(a[a2], 80)
 
@@ -226,26 +259,33 @@ def test_update_instrument(self):
     countries = set(self.countries)
     c1 = countries.pop()
     c2 = countries.pop()
-    self.assertTrue(self.dao.update_instrument_country_allocations(instrument.name, {
-        'countries': [{'country': c1, 'ratio': 20}, {'country': c2, 'ratio': 80}]}))
-    c = {t: r for t, r in self.dao.get_country_allocation(instrument_name=instrument.name)}
+    self.assertTrue(
+        self.dao.update_instrument_country_allocations(
+            instrument.name,
+            {"countries": [{"country": c1, "ratio": 20}, {"country": c2, "ratio": 80}]},
+        )
+    )
+    c = {
+        t: r
+        for t, r in self.dao.get_country_allocation(instrument_name=instrument.name)
+    }
     self.assertEqual(c[c1], 20)
     self.assertEqual(c[c2], 80)
 
     # insert new instrument
-    name = instrument.name + ':this is a new instrument'
+    name = instrument.name + ":this is a new instrument"
     # before insert
     self.assertRaises(StopIteration, self.dao.get_instruments(name).__next__)
     # now insert and verify
-    payload.pop('expense')
-    payload['type'] = 'Stock'
+    payload.pop("expense")
+    payload["type"] = "Stock"
     self.assertTrue(self.dao.update_instrument(name, payload))
     i = self.dao.get_instruments(name).__next__()
-    self.assertEqual(i['type'], 'Stock')
-    self.assertEqual(i['currency'], ccy)
-    self.assertEqual(i['broker'], broker)
-    self.assertEqual(i['url'], url)
+    self.assertEqual(i["type"], "Stock")
+    self.assertEqual(i["currency"], ccy)
+    self.assertEqual(i["broker"], broker)
+    self.assertEqual(i["url"], url)
 
 
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
