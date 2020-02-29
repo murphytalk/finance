e3ade28db0dad05bc8173d20587c20fe75757688
Mu Lu <murphytalk@gmail.com>
2020-02-29 12:06:23 +0900

    docker script

--- bin/docker-run.sh
+++ bin/docker-run.sh
@@ -2,5 +2,4 @@
 
 export FINANCE_DB=/data/finance.db
 export SERVER_HOST=`ifconfig eth0 |grep 'inet addr'|sed 's/.*addr:\(.*\) Bcast.*$/\1/g'`
-exec python /finance/src/runserver.py 8080
-
+exec python3 /finance/src/runserver.py 8080
