ea02ceed6ad581243b13f0d661e428bbe40b6157
Mu Lu <murphytalk@gmail.com>
2020-06-20 22:27:32 +0900

    finance-ng the angular app

---
+++ finance-ng/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IgxNavigationDrawerModule, IgxNavbarModule, IgxLayoutModule, IgxRippleModule, IgxGridModule } from '@infragistics/igniteui-angular';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { StocksGridComponent } from './stocksgrid/stocksgrid.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    StocksGridComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    HammerModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    IgxNavigationDrawerModule,
+    IgxNavbarModule,
+    IgxLayoutModule,
+    IgxRippleModule,
+    IgxGridModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
