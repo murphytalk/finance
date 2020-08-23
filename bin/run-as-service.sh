d007a1d0f64a39155d4aa41d25b272e8930606a5
Mu Lu <murphytalk@gmail.com>
2020-08-23 17:59:41 +0900

    script to start finance as daemon on gentoo

--- bin/run-as-service.sh
+++ bin/run-as-service.sh
@@ -2,9 +2,14 @@
 #https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euo pipefail
 
+# for OpenRC
+# make symlink /etc/local.d/finance.start and /etc/local.d/finance.stop to this file
+
 if [ ! -L $0 ];then
     echo "Do not run this script directly, make a symlink and name it as action"
     exit 1
 fi
+realpath=$(readlink $0)
+cmd=$(echo `basename $0`|sed -r 's/.*\.(.*)$/\1/g')
+sudo -u mu `dirname $realpath`/run-on-deepwater.sh $cmd
 
-sudo -u mu `dirname $0`/run-on-deepwater.sh `basename $0`
