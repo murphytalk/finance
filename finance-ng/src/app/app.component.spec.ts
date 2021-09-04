328368e196ccbec33a1d047ece7ed2b9e59f4af4
Mu Lu <murphytalk@gmail.com>
2021-09-04 23:03:02 +0900

    fix unit test

--- finance-ng/src/app/app.component.spec.ts
+++ finance-ng/src/app/app.component.spec.ts
@@ -1,16 +1,20 @@
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { LoggerTestingModule } from 'ngx-logger/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        LoggerTestingModule
       ],
       declarations: [
         AppComponent
       ],
+      schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
 
@@ -24,6 +28,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('finance-ng app is running!');
+    expect(compiled.querySelector('button').textContent).toContain('menu');
   });
 });
